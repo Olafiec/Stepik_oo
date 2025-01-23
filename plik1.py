6736119fc20d29ccc507b5a92eccc7f8ea61f906
Olafiec <mister.olafo@gmail.com>
2025-01-23 10:28:22 +0100

    Dodanie Testów i plików python

---
+++ plik1.py
@@ -0,0 +1,10 @@
+
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+if __name__ == "__main__":
+    print("Dodawanie 3 + 5:", add(3, 5))
+    print("Mnożenie 3 * 5:", multiply(3, 5))
